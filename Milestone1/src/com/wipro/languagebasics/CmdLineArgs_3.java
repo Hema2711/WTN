599272ae9c6812064ef241924acc24ad34a14cf8
Sree <Sree@DESKTOP-VIL43VF>
2020-06-12 03:39:30 +0530

    wipro practise qs 3

---
+++ Milestone1/src/com/wipro/languagebasics/CmdLineArgs_3.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+
+public class CmdLineArgs_3 {
+public static void main(String args[])
+{
+	int a,b,c;
+	a=Integer.parseInt(args[0]);
+	b=Integer.parseInt(args[1]);
+	c=a+b;
+			System.out.println("The sum of " +a+ " and " +b+ " is " +c);
+}
+}
