0083061c7fece3e1e2fcb9793be20aa6427cb55c
Sree <Sree@DESKTOP-VIL43VF>
2020-06-12 09:23:26 +0530

    Program to find whether the num is Even or odd

---
+++ Milestone1/src/flowcontrol/Example_2.java
@@ -0,0 +1,18 @@
+package flowcontrol;
+
+public class Example_2 {
+	public static void main(String args[])
+	{
+		int a;
+		a=Integer.parseInt(args[0]);
+		if(a%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+	}
+
+}
