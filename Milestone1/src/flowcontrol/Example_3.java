9426cfb5dc77599d902caf8b163ad6981ef00b14
Sree <Sree@DESKTOP-VIL43VF>
2020-06-12 10:01:15 +0530

    Program to find whether commandline arguments are given are not.

---
+++ Milestone1/src/flowcontrol/Example_3.java
@@ -0,0 +1,21 @@
+package flowcontrol;
+
+public class Example_3 {
+	public static void main(String args[])
+	{
+		int a=args.length;
+      if(a==0)
+      {
+    	  System.out.println("No Values");
+      }
+      else
+      {
+    	  System.out.print(args[0]);
+    	  for(int i=1;i<a;i++)
+    	  {
+    		  System.out.print(","+args[i]);
+    	  }
+      }
+	}
+
+}
