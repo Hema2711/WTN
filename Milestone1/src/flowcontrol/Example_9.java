227f40c76aa68cb65fb91ddf232ae5f7a0ee6b15
Sree <Sree@DESKTOP-VIL43VF>
2020-06-14 08:41:20 +0530

    month name

---
+++ Milestone1/src/flowcontrol/Example_9.java
@@ -0,0 +1,52 @@
+package flowcontrol;
+
+public class Example_9 {
+	public static void main(String args[])
+	{
+		int month;
+		month=Integer.parseInt(args[0]);
+		switch(month)
+		{	
+		case 1: 
+			System.out.println("January");
+			break;
+		case 2: 
+			System.out.println("February");
+			break;
+		case 3: 
+			System.out.println("March");
+			break;
+		case 4: 
+			System.out.println("Apirl");
+			break;
+		case 5: 
+			System.out.println("May");
+			break;
+		case 6: 
+			System.out.println("June");
+			break;
+		case 7: 
+			System.out.println("July");
+			break;
+		case 8: 
+			System.out.println("Aguest");
+			break;
+		case 9: 
+			System.out.println("september");
+			break;
+		case 10: 
+			System.out.println("October");
+			break;
+		case 11: 
+			System.out.println("November");
+			break;
+		case 12: 
+			System.out.println("December");
+			break;
+		default:
+			System.out.println("Invalid month");
+		}
+		
+	}
+
+}
