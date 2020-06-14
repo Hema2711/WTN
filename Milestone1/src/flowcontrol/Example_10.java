a8c06619cd7fb2ce9a07bec67ac157b8ce8abc02
Sree <Sree@DESKTOP-VIL43VF>
2020-06-14 09:12:34 +0530

    printing numbers from 1 to 10 in a single row

---
+++ Milestone1/src/flowcontrol/Example_10.java
@@ -0,0 +1,14 @@
+package flowcontrol;
+
+public class Example_10 {
+	public static void main(String args[])
+	{
+		int a,i;
+		a=Integer.parseInt(args[0]);
+		for(i=1;i<=a;i++)
+		{
+			System.out.print(i+"\t");
+		}
+	}
+
+}
