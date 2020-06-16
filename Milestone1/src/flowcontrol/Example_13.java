e1a6ab1fb71febae0266f5f154d26a040f0d2c7d
Sree <Sree@DESKTOP-VIL43VF>
2020-06-16 07:41:48 +0530

    Prime numbers between 10 and 99.

---
+++ Milestone1/src/flowcontrol/Example_13.java
@@ -0,0 +1,25 @@
+package flowcontrol;
+
+public class Example_13 {
+public static void main(String args[])
+{
+	int i;
+	for(i=10;i<=99;i++)
+	{
+		int c=0;
+		for(int j=2;j*j<=i;j++)
+		{
+		if(i%j==0)
+		{
+			c++;
+		    break;
+        }
+		}
+	
+	if(c==0)
+	 {
+		System.out.println(i);
+	 }
+	}
+}
+}
